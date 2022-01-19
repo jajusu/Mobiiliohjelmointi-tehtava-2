95da27a322b2ec7f77de0381135453ff36ef61ed
J <j.suomaki@gmail.com>
2022-01-19 13:20:54 +0200

    Valmis

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState } from 'react';
+import { StyleSheet, View, Button, Alert, TextInput, Text } from 'react-native';
+let luku=Math.floor(Math.random() * 100) + 1;
+let maara=0;
 export default function App() {
+  const [text, setText] = useState('');
+  const [infoteksti, setInfo] = useState('Guess a number between 1 and 100');
+  const [arvaus, setArvaus] = useState('0');
+
+  const arvaa = () => { 
+    if (arvaus==luku){
+      maara++;
+      Alert.alert('You guessed the number in ' + maara + ' guesses');
+      setInfo("Guess a number between 1 and 100");
+      setArvaus("");
+      luku=Math.floor(Math.random() * 100) + 1;
+      maara=0;
+    }
+    else if (arvaus>luku){
+      setInfo("Your guess " + arvaus + " is too high.");
+      maara++;
+    }
+    else if (arvaus<luku){
+      setInfo("Your guess " + arvaus + " is too low.");
+      maara++;
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.teksti}> {infoteksti} </Text>
+      <TextInput style={styles.input} 
+        onChangeText={text => setArvaus(text)}
+        value={arvaus}
+        keyboardType={'numeric'}/>
+      <Button onPress={arvaa} title="Make guess" />
+      {/* <Text style={styles.teksti}>Oikea {luku} määrä {maara}</Text> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +48,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  teksti:{
+    width:250, 
+    borderColor: 'gray', 
+    margin: 5,
+    textAlign: 'center'
+  },
+  input : {
+    width:50  , 
+    borderColor: 'gray', 
+    borderWidth: 1,
+    margin: 5,
+    textAlign: 'center'
+  }
 });
